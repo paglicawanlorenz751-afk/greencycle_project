9366d4e7e80805c8b5f18ea5e24f998e69788ae4
paglicawanlorenz751-afk <paglicawanlorenz751@gmail.com>
2026-03-08 12:15:25 +0000

    Initial GreenCycle website

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>GreenCycle</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<h1>GreenCycle</h1>
+<p>Turn Waste into Resources</p>
+
+<a href="login.php">Login</a>
+<a href="register.php">Register</a>
+
+</body>
+
+</html>
